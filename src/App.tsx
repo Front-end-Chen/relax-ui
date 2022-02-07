951bda531d8efea8c81c4cdf967389dd420bee8a
Front-end-Chen <huangcj_it@163.com>
2022-02-07 17:32:03 +0800

    ui库项目初始化，公共样式定义和样式重置

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <h1>Hello world</h1>
+        <h2>Hello world</h2>
+        <h3>Hello world</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
